7895d879a970a0fa5fe219ade85b25b5ab4fbe8c
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2023-06-01 16:31:21 +0900

    ✨ Link先の修正

--- app/Header.tsx
+++ app/Header.tsx
@@ -10,7 +10,6 @@ const Header: React.FC = () => {
     <div className="bg-white lg:pb-6">
       <div className="max-w-screen-2xl px-2 md:px-4 mx-auto">
         <header className="flex justify-between items-center py-4">
-          {/* 1. トップページへのリンク */}
           <Link
             href="/"
             className="inline-flex items-center text-black-800 text-xl font-bold gap-2.5"
@@ -33,11 +32,9 @@ const Header: React.FC = () => {
             </svg>
             {title}
           </Link>
-          {/* 2. 画面幅が768px未満の場合は非表示 */}
           <nav className="hidden md:flex gap-12">
-            {/* 3. リンク先は未実装のためトップページに遷移 */}
             <Link
-              href="/"
+              href="/notes"
               className="text-gray-600 hover:text-pink-500 active:text-pink-700 text-lg font-semibold transition duration-100"
             >
               Memo
