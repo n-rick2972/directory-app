bae0374fe00adaaf0def864fdcbfdd6e28a8da6a
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2023-06-01 17:27:42 +0900

    🐛 Application error対応

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/**
+ * @type { import("next").NextConfig}
+ */
+const config = {
+  experimental: {
+    swcLoader: true,
+    swcMinify: true,
+    cpus: 4,
+  },
+};
+module.exports = config;
